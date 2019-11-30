b4fdca8a7b4c7630647fd44732faa98736304a29
Ildar Masagutov <39755178+mazecore@users.noreply.github.com>
2019-11-30 03:20:48 -0500

    app

---
+++ Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.bluetoothlegatt;
+
+import java.util.HashMap;
+
+/**
+ * This class includes a small subset of standard GATT attributes for demonstration purposes.
+ */
+public class SampleGattAttributes {
+    private static HashMap<String, String> attributes = new HashMap();
+    public static String HEART_RATE_MEASUREMENT = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
+    public static String CLIENT_CHARACTERISTIC_CONFIG = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E";
+
+    static {
+        // Sample Services.
+        attributes.put("6E400002-B5A3-F393-E0A9-E50E24DCCA9E", "Heart Rate Service");
+        attributes.put("6E400002-B5A3-F393-E0A9-E50E24DCCA9E", "Device Information Service");
+        // Sample Characteristics.
+        attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
+        attributes.put("6E400002-B5A3-F393-E0A9-E50E24DCCA9E", "Manufacturer Name String");
+    }
+
+    public static String lookup(String uuid, String defaultName) {
+        String name = attributes.get(uuid);
+        return name == null ? defaultName : name;
+    }
+}
